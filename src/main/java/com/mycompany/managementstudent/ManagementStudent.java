80eb00f4789be39f0f2707a6b24bf8cdb188c9bb
Quoc An <quocan@gmail.com>
2024-10-30 13:42:48 +0700

    test push first

---
+++ src/main/java/com/mycompany/managementstudent/ManagementStudent.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.managementstudent;
+
+/**
+ *
+ * @author Admin
+ */
+public class ManagementStudent {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
